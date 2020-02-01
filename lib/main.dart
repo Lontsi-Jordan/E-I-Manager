801b229fd02435585f179edffb9a7b62d971dd64
Jordan Lontsi <jordylontsi@gmail.com>
2020-02-01 09:09:21 +0100

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:budget_manager/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-I Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        brightness: Brightness.light
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
